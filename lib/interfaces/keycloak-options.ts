80279e1f9d251e7f1131e4ec4febaa57259e6593
Mauricio Gemelli Vigolo <mauriciogv@sanepar.com.br>
2017-09-06 18:31:04 -0300

    feat: cont development

--- lib/interfaces/keycloak-options.ts
+++ lib/interfaces/keycloak-options.ts
@@ -3,6 +3,6 @@ import { KeycloakInitOptions } from 'keycloak-js';
 import { KeycloakConfig } from './keycloak-config';
 
 export interface KeycloakOptions {
-  config: KeycloakConfig;
+  config: string | KeycloakConfig;
   initOptions: KeycloakInitOptions;
 }
