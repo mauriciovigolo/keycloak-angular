11a42473eafdec2edcfe93de2f9e4c2f8073aead
Mauricio Gemelli Vigolo <mauriciogv@sanepar.com.br>
2017-10-02 18:00:00 -0300

    style: added license info in files

--- lib/keycloak.module.ts
+++ lib/keycloak.module.ts
@@ -1,7 +1,14 @@
+/**
+* @license
+* Copyright Mauricio Gemelli Vigolo. All Rights Reserved.
+*
+* Use of this source code is governed by a MIT-style license that can be
+* found in the LICENSE file at https://github.com/mauriciovigolo/keycloak-angular/LICENSE
+*/
 import { NgModule } from '@angular/core';
-import { KeycloakAngular } from './services';
+import { KeycloakAngularService } from './services';
 
 @NgModule({
-  providers: [KeycloakAngular]
+  providers: [KeycloakAngularService]
 })
 export class KeycloakModule {}
