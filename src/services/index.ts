ce83de2a4a0a704402ba8c64acde6231eee9b70f
Mauricio Gemelli Vigolo <mauriciovigolo@gmail.com>
2017-10-04 18:01:01 -0300

    fix: changed keycloak instance to static.

--- src/services/index.ts
+++ src/services/index.ts
@@ -5,5 +5,5 @@
 * Use of this source code is governed by a MIT-style license that can be
 * found in the LICENSE file at https://github.org/mauriciovigolo/keycloak-angular/LICENSE
 */
-export * from './keycloak-angular.service';
+export * from './keycloak.service';
 export * from './keycloak-auth-guard.service';
