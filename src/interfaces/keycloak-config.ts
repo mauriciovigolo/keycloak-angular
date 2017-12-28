a135a4b8516d55a6e25bc3fdc5969b7266fd1077
Mauricio Gemelli Vigolo <mauriciovigolo@gmail.com>
2017-12-28 15:02:43 -0200

    feat: new library version, including improvements on docs, interfaces and dependencies version.
    - Updated keycloak-js version to 3.4.2.
    - Updated versions of several devDependencies.
    - Added jsdocs for all classes and interfaces. Now it will be possible to generate the API documentation.
    - Added a new property, regarding the contribution PR #2 from @jmparra. Created the type credentials.
    - Bump library version to 1.2.0.

--- src/interfaces/keycloak-config.ts
+++ src/interfaces/keycloak-config.ts
@@ -1,29 +1,43 @@
 /**
  * @license
- * Copyright Mauricio Gemelli Vigolo.
+ * Copyright Mauricio Gemelli Vigolo and contributors.
  *
  * Use of this source code is governed by a MIT-style license that can be
  * found in the LICENSE file at https://github.com/mauriciovigolo/keycloak-angular/LICENSE
  */
+
+/**
+ * Credentials type. Should be used when The Access Type is configured as Confidential, as
+ * mentioned in the keycloak's documentation:
+ * http://www.keycloak.org/docs/latest/securing_apps/index.html#_configuring_a_client_for_use_with_client_registration_cli
+ */
+export type Credentials = {
+  /**
+   * Secret or Signed JWT. Please, caution where you store this sensitive information!
+   */
+  secret: string;
+};
+
+/**
+ * This is the interface containing the attributes for the keycloak configuration in case
+ * you don't specify a keycloak.json file in your project.
+ */
 export interface KeycloakConfig {
   /**
-   *
+   * Keycloak server url, for example: http://localhost:8080/auth
    */
   url: string;
   /**
-   *
+   * Realm name, ie.: myrealm
    */
   realm: string;
   /**
-   *
+   * Client ID, ie.: myapp
    */
   clientId: string;
   /**
-   *
+   * The credentials object contains the secret property that should be used depending on
+   * which flow and access type was chosen.
    */
-  clientSecret?: string;
-  /**
-   *
-   */
-  credentials?: any;
+  credentials?: Credentials;
 }
--- src/interfaces/keycloak-options.ts
+++ src/interfaces/keycloak-options.ts
@@ -8,17 +8,27 @@
 import { KeycloakInitOptions } from './keycloak-init-options';
 import { KeycloakConfig } from './keycloak-config';
 
+/**
+ * keycloak-angular initialization options.
+ */
 export interface KeycloakOptions {
   /**
-   *
+   * Configs to init the keycloak-js library. If undefined, will look for a keycloak.json file
+   * at root of the project.
+   * If not undefined, can be a string meaning the url to the keycloak.json file or an object
+   * of {@link KeycloakConfig}. Use this configuration if you want to specify the keycloak server,
+   * realm, clientId. This is usefull if you have different configurations for production, stage
+   * and development environments. Hint: Make use of Angular environment configuration.
    */
   config?: string | KeycloakConfig;
   /**
-   *
+   * Options to initialize the adapter. Used by keycloak-js.
    */
   initOptions?: KeycloakInitOptions;
   /**
-   *
+   * String Array to exclude the urls that should not have the Authorization Header automatically
+   * added. This library makes use of Angular Http Interceptor, to automatically add the Bearer
+   * token to the request.
    */
   bearerExcludedUrls?: string[];
 }
