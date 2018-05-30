dea0edffafea2fd338013a17febe942e5c158068
Mauricio Gemelli Vigolo <mauriciovigolo@gmail.com>
2018-05-30 09:52:11 -0300

    docs: added keycloak-heroes example

---
+++ examples/keycloak-heroes/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, APP_INITIALIZER } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { ClarityModule } from 'clarity-angular';
+import { KeycloakAngularModule, KeycloakService } from 'keycloak-angular';
+
+import { AppComponent, HeroesComponent, HomeComponent } from './components';
+import { HeroesService } from './services';
+import { AppRoutingModule } from './app-routing.module';
+import { AppAuthGuard } from './app.authguard';
+import { initializer } from './utils/app-init';
+
+@NgModule({
+  declarations: [AppComponent, HeroesComponent, HomeComponent],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    ClarityModule.forRoot(),
+    KeycloakAngularModule,
+    AppRoutingModule
+  ],
+  providers: [
+    HeroesService,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializer,
+      multi: true,
+      deps: [KeycloakService]
+    }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
---
+++ examples/keycloak-heroes/src/app/services/heroes/hero.ts
@@ -0,0 +1,10 @@
+export interface Hero {
+  id?: number;
+  name?: string;
+  localized_name?: string;
+  primary_attr?: string;
+  attack_type?: string;
+  roles?: string[];
+  legs?: number;
+  image_url?: string;
+}
