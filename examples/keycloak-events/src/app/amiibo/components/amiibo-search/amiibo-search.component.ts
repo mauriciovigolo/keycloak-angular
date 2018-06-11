d0b2fe1a33f2afefa8ba441f1157a8f63fd6456b
Mauricio Gemelli Vigolo <mauriciovigolo@gmail.com>
2018-06-11 06:20:03 -0300

    docs: added amiibo component, service and module to keycloak-events.
    - countries module was removed.
    - amiibo module will be placed as secured content instead of countries.
    - angular tests setup.

---
+++ examples/keycloak-events/src/app/amiibo/components/amiibo-search/amiibo-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-amiibo-search',
+  templateUrl: './amiibo-search.component.html',
+  styleUrls: ['./amiibo-search.component.css']
+})
+export class AmiiboSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
