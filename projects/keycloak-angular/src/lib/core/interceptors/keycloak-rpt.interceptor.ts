69c5c709528ec4fa31ea5d299a2c131f7195b9dc
Marcel Német <marcel.nemet@swisscom.com>
2018-07-04 08:31:12 +0200

    Change Copyright of RPT interceptor

--- projects/keycloak-angular/src/lib/core/interceptors/keycloak-rpt.interceptor.ts
+++ projects/keycloak-angular/src/lib/core/interceptors/keycloak-rpt.interceptor.ts
@@ -1,6 +1,6 @@
 /**
  * @license
- * Copyright Marcel Német and contributors.
+ * Copyright Swisscom (Schweiz) AG and contributors.
  *
  * Use of this source code is governed by a MIT-style license that can be
  * found in the LICENSE file at https://github.com/mauriciovigolo/keycloak-angular/LICENSE
