58ec32045a329535876dde4973b24fcd0e07e8e3
Mauricio Gemelli Vigolo <mauriciovigolo@gmail.com>
2018-08-01 08:06:58 -0300

    docs(examples): structuring and renaming the keycloak-server for demo.

--- examples/keycloak-server-demo/core/utils.js
+++ examples/keycloak-server-demo/core/utils.js
@@ -1,5 +1,13 @@
-const utils = {
-  serverInitCallback: function() {
+/**
+ * @license
+ * Copyright Mauricio Gemelli Vigolo and contributors.
+ *
+ * Use of this source code is governed by a MIT-style license that can be
+ * found in the LICENSE file at https://github.com/mauriciovigolo/keycloak-angular/LICENSE
+ */
+
+module.exports = (function() {
+  const serverInitCallback = function() {
     console.log(' _               _         _                      _         ');
     console.log('| |_ ___ _ _ ___| |___ ___| |_    ___ ___ ___ _ _| |___ ___ ');
     console.log("| '_| -_| | |  _| | . | .'| '_|  | .'|   | . | | | | .'|  _|");
@@ -9,7 +17,9 @@ const utils = {
     console.log('Keycloak Server Example Initialized on Port 3000');
     console.log('---------------');
     console.log('For a complete list of API services, visit: http://localhost:3000/');
-  }
-};
+  };
 
-module.exports = utils;
+  return {
+    serverInitCallback
+  };
+})();
--- examples/keycloak-server-demo/index.js
+++ examples/keycloak-server-demo/index.js
@@ -1,6 +1,15 @@
 #!/usr/bin/env node
+
+/**
+ * @license
+ * Copyright Mauricio Gemelli Vigolo and contributors.
+ *
+ * Use of this source code is governed by a MIT-style license that can be
+ * found in the LICENSE file at https://github.com/mauriciovigolo/keycloak-angular/LICENSE
+ */
+
 const express = require('express');
-const utils = require('./utils');
+const utils = require('./core/utils');
 
 const app = express();
 
