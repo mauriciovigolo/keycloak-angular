2f1e1c705132aa831c8ef0dd7dc7dcf99f0cbbbe
Mauricio Gemelli Vigolo <mauriciovigolo@gmail.com>
2018-08-08 01:42:52 -0300

    docs(examples): node server demo src folder refactor

--- examples/keycloak-server-demo/core/config/log.config.js
+++
@@ -1,9 +0,0 @@
-const { createLogger, format, transports } = require('winston');
-
-const logger = createLogger({
-  level: 'info',
-  format: format.prettyPrint(),
-  transports: [new transports.Console()]
-});
-
-module.exports = logger;
