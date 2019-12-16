0eb1c6dc08033dc13f668fd83e254d173917d3e7
Mauricio Gemelli Vigolo <mauriciovigolo@gmail.com>
2019-12-16 22:45:09 +0100

    fix(travis): fixes travis configuration

--- projects/keycloak-angular/karma.conf.js
+++ projects/keycloak-angular/karma.conf.js
@@ -26,6 +26,12 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: false
+    singleRun: false,
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    }
   });
 };
