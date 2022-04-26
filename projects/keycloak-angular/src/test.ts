65ea39c76b8198c58c7046c5665293a0a0fe91db
XhmikosR <xhmikosr@gmail.com>
2022-04-26 14:49:19 +0300

    Fix license link (#394)

--- projects/keycloak-angular/src/test.ts
+++ projects/keycloak-angular/src/test.ts
@@ -3,7 +3,7 @@
  * Copyright Mauricio Gemelli Vigolo and contributors.
  *
  * Use of this source code is governed by a MIT-style license that can be
- * found in the LICENSE file at https://github.com/mauriciovigolo/keycloak-angular/LICENSE
+ * found in the LICENSE file at https://github.com/mauriciovigolo/keycloak-angular/blob/master/LICENSE.md
  */
 
 import 'zone.js';
