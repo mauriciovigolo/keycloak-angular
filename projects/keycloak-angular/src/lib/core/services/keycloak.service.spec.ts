mauriciovigolo/keycloak-angular

commit cb4100cd8530cbd21f851f4ac6f4d207daf4328c

fix: updateToken when an exception happens
- Related to issue #409

projects/keycloak-angular/src/lib/core/services/keycloak.service.ts
@@ -366,7 +366,7 @@ export class KeycloakService {
    * A boolean that indicates if the user is logged in.
    */
   async isLoggedIn(): Promise<boolean> {
-    return this._instance.authenticated;
+    return await this._instance.authenticated;
   }
 
   /**
@@ -394,23 +394,27 @@ export class KeycloakService {
    * Promise with a boolean indicating if the token was succesfully updated.
    */
   public async updateToken(minValidity = this._updateMinValidity) {
-    // TODO: this is a workaround until the silent refresh (issue #43)
-    // is not implemented, avoiding the redirect loop.
-    if (this._silentRefresh) {
-      if (this.isTokenExpired()) {
-        throw new Error(
-          'Failed to refresh the token, or the session is expired'
-        );
+    try {
+      // TODO: this is a workaround until the silent refresh (issue #43)
+      // is not implemented, avoiding the redirect loop.
+      if (this._silentRefresh) {
+        if (this.isTokenExpired()) {
+          throw new Error(
+            'Failed to refresh the token, or the session is expired'
+          );
+        }
+
+        return true;
       }
 
-      return true;
-    }
+      if (!this._instance) {
+        throw new Error('Keycloak Angular library is not initialized.');
+      }
 
-    if (!this._instance) {
-      throw new Error('Keycloak Angular library is not initialized.');
+      return await this._instance.updateToken(minValidity);
+    } catch (error) {
+      return false;
     }
-
-    return this._instance.updateToken(minValidity);
   }
 
   /**

projects/keycloak-angular/src/lib/core/interceptors/keycloak-bearer.interceptor.ts
@@ -96,8 +96,8 @@ export class KeycloakBearerInterceptor implements HttpInterceptor {
       this.conditionallyUpdateToken(req),
       this.keycloak.isLoggedIn()
     ]).pipe(
-      mergeMap(([_, loggedIn]: [boolean, boolean]) =>
-        loggedIn
+      mergeMap(([_, isLoggedIn]) =>
+        isLoggedIn
           ? this.handleRequestWithTokenHeader(req, next)
           : next.handle(req)
       )

commit 142faebc1cfcd2b1a3084c59f1518a1cf0081ff3

fix: modify updateToken so it does not swallow errors
Fixes #433. Reverts regression introduced with #412.

projects/keycloak-angular/src/lib/core/services/keycloak.service.ts
@@ -398,23 +398,23 @@ export class KeycloakService {
    * Promise with a boolean indicating if the token was succesfully updated.
    */
   public async updateToken(minValidity = this._updateMinValidity) {
+    // TODO: this is a workaround until the silent refresh (issue #43)
+    // is not implemented, avoiding the redirect loop.
+    if (this._silentRefresh) {
+      if (this.isTokenExpired()) {
+        throw new Error(
+          'Failed to refresh the token, or the session is expired'
+        );
+      }
+
+      return true;
+    }
+
+    if (!this._instance) {
+      throw new Error('Keycloak Angular library is not initialized.');
+    }
+
     try {
-      // TODO: this is a workaround until the silent refresh (issue #43)
-      // is not implemented, avoiding the redirect loop.
-      if (this._silentRefresh) {
-        if (this.isTokenExpired()) {
-          throw new Error(
-            'Failed to refresh the token, or the session is expired'
-          );
-        }
-
-        return true;
-      }
-
-      if (!this._instance) {
-        throw new Error('Keycloak Angular library is not initialized.');
-      }
-
       return await this._instance.updateToken(minValidity);
     } catch (error) {
       return false;

projects/keycloak-angular/src/lib/core/services/keycloak.service.spec.ts
@@ -77,5 +77,14 @@ describe('KeycloakService', () => {
         expect(token).toEqual('testToken');
       }
     ));
+
+    it('should throw error if updateToken is called before initialization', inject(
+      [KeycloakService],
+      async (service: KeycloakService) => {
+        await expectAsync(service.updateToken()).toBeRejectedWithError(
+          /not initialized/
+        );
+      }
+    ));
   });
 });