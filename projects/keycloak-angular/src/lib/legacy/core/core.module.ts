683f27646e7aab44b8a7cd2e0979da26752368d4
Luca Gobbi <80841205+lucagobbi@users.noreply.github.com>
2025-06-11 03:55:46 +0200

    docs: fix incorrect migration guide URL (#639)
    Co-authored-by: luca.gobbi <luca.gobbi@niuma.it>

--- projects/keycloak-angular/src/lib/legacy/core/core.module.ts
+++ projects/keycloak-angular/src/lib/legacy/core/core.module.ts
@@ -16,7 +16,7 @@ import { KeycloakBearerInterceptor } from './interceptors/keycloak-bearer.interc
 /**
  * @deprecated NgModules are deprecated in Keycloak Angular and will be removed in future versions.
  * Use the new `provideKeycloak` function to load Keycloak in an Angular application.
- * More info: https://github.com/mauriciovigolo/keycloak-angular/docs/migration-guides/v19.md
+ * More info: https://github.com/mauriciovigolo/keycloak-angular/blob/main/docs/migration-guides/v19.md
  */
 @NgModule({
   imports: [CommonModule],
